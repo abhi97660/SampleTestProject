21c86c931bf38084edb3db36110fc235275c5da3
Abhilash <abhi97660@gmail.com>
2018-07-06 13:30:56 +0530

    second commit

--- demo.py
+++ demo.py
@@ -1,4 +1,4 @@
 import nltk
 from nltk import word_tokenize
-sentence ="Hello this is testing project on the github"
+sentence ="Hello this is testing project on the github. This is done for testing in jenkins"
 print(word_tokenize(sentence))
