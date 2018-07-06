da9cc07cd68b69c5bf1a4ecbd4ab5696fad9c136
Abhilash <abhi97660@gmail.com>
2018-07-06 13:40:47 +0530

    third commit

--- demo.py
+++ demo.py
@@ -1,2 +1,2 @@
-sentence ="Hello this is testing project on the github. This is done for testing in jenkins"
+sentence ="Hello this is testing project on the github. This is done for testing in jenkins. CI Cd"
 print(sentence)
