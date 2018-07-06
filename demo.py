eefd7f8eec53363d9d9a13b65a62bad2cb08763b
Abhilash <abhi97660@gmail.com>
2018-07-06 13:34:48 +0530

    removing extra libs

--- demo.py
+++ demo.py
@@ -1,4 +1,2 @@
-import nltk
-from nltk import word_tokenize
 sentence ="Hello this is testing project on the github. This is done for testing in jenkins"
-print(word_tokenize(sentence))
+print(sentence)
