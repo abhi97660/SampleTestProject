d4bce10047cb656254d6a9d3bbc51893d02559bf
Abhilash <abhi97660@gmail.com>
2018-07-06 14:26:20 +0530

    seventh commit

--- demo.py
+++ demo.py
@@ -1,4 +1,4 @@
-import nltk
-from nltk import word_tokenize
+# import nltk
+# from nltk import word_tokenize
 sentence ="Hello this is testing project on the github. This is done for testing in jenkins. CI Cd integration"
-print(word_tokenize(sentence))
+# print(word_tokenize(sentence))
